38fc9acda689aa57ed612298069eab0915f171c6
Jos Koomen <ik@joskoomen.nl>
2019-10-23 18:08:58 +0200

    test component

--- src/__tests__/Component.spec.js
+++ src/__tests__/Component.spec.js
@@ -1,12 +1,14 @@
 // @flow
 import Component from '../components/Component';
+import NotificationController from '../notifications/NotificationController';
 
 let componentA: Component;
 let componentB: Component;
 
 const createView = () => {
   let html = '<div id="componentA" data-component="componentA"></div>';
-  html += '<div id="componentB" data-component="componentB"></div>';
+  html += '<div id="componentB" data-component="componentB">';
+  html += '<span>Child element</span></div>';
   window.document.body.innerHTML = html;
 };
 
@@ -24,7 +26,7 @@ test('Component has a name and element', () => {
   expect(componentA.name).not.toBeUndefined();
 });
 
-test('Components registers Events', () => {
+test('Components registers events', () => {
   const handler: function = jest.fn();
   componentA.addEventListener('click', handler);
   expect(componentA.events).toHaveLength(1);
@@ -44,4 +46,34 @@ test('Components handles notifications', () => {
     componentA.notify('test');
     expect(listener).toHaveBeenCalledTimes(0);
   }, 0);
+});
+
+test('Components renders templates', () => {
+  expect(componentB.el.children).toHaveLength(1);
+  componentB.render({});
+  expect(componentB.el.children).toHaveLength(0);
+});
+
+describe('Component destroy', () => {
+
+  it('shouldn\'t have any listeners', () => {
+    const nc: NotificationController = NotificationController.getInstance();
+    const handler: function = jest.fn();
+
+    componentA.addEventListener('click', handler);
+    componentB.addEventListener('click', handler);
+    componentA.addListener('destroy_test');
+    componentB.addListener('destroy_test');
+
+    expect(nc.listeners.length).toBeGreaterThanOrEqual(1);
+    expect(componentA.events).toHaveLength(1);
+    expect(componentB.events).toHaveLength(1);
+
+    componentA.destroy();
+    componentB.destroy();
+
+    expect(nc.listeners).toHaveLength(0);
+    expect(componentA.events).toHaveLength(0);
+    expect(componentB.events).toHaveLength(0);
+  });
 });
