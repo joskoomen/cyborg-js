6362da51c653057e463062be43377dd65b747fcf
Jos Koomen <jos@yourpersonalagency.nl>
2020-10-29 15:32:07 +0100

    [3.0.5] improved walkDom functionality for nested components

--- src/functions/walkDom.ts
+++ src/functions/walkDom.ts
@@ -1,11 +1,12 @@
 export function walkDom(pEl: Element | null, pCallback: Function): void {
   if (pEl) {
-    pCallback(pEl)
-    pEl = pEl.firstElementChild
-
+    pCallback(pEl);
+    pEl = pEl.firstElementChild;
     while (pEl) {
-      walkDom(pEl, pCallback)
-      pEl = pEl.nextElementSibling
+      if (!pEl.hasAttribute('data-component')) {
+        walkDom(pEl, pCallback);
+      }
+      pEl = pEl.nextElementSibling;
     }
   }
 }
